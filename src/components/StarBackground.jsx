02fa775c05a6856dc9426d4ed0832adc97f91cfe
khasanboyurmonovdev <khasanboyurmonov1998@yahoo.com>
2025-11-19 04:14:56 +0900

    feat: created components and lib folders

---
+++ src/components/StarBackground.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+// id, size, x, y, opacity, animationDuration
+// id, size, x, y, delay, animationDuration
+
+export const StarBackground = () => {
+  const [stars, setStars] = useState();
+  return <></>;
+};
